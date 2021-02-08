e924984bb484f32ee69035b99369e44b0efc9bd8
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-02-08 23:27:51 +0100

    moved footer content into seperate component

---
+++ components/TheFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="bg-white">
+    <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <div class="mt-8 md:mt-0">
+        <p class="text-base text-center text-gray-400">
+          © {{ new Date().getFullYear() }} Z-Works. All rights reserved.
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
     <Nuxt />
-    <footer class="bg-white">
-      <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div class="mt-8 md:mt-0">
-          <p class="text-base text-center text-gray-400">
-            © 2020 Z-Works. All rights reserved.
-          </p>
-        </div>
-      </div>
-    </footer>
+    <TheFooter />
   </div>
 </template>
