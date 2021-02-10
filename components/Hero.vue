b96df1bdc71de790f69eca06dac000df91af4592
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-02-10 08:53:35 +0100

    added missing alt text

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="relative bg-white overflow-hidden">
-    <div class="max-w-7xl mx-auto">
+  <div class="relative overflow-hidden bg-white">
+    <div class="mx-auto max-w-7xl">
       <div
         class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
       >
         <svg
-          class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+          class="absolute inset-y-0 right-0 hidden w-48 h-full text-white transform translate-x-1/2 lg:block"
           fill="currentColor"
           viewBox="0 0 100 100"
           preserveAspectRatio="none"
@@ -14,7 +14,7 @@
           <polygon points="50,0 100,0 50,100 0,100" />
         </svg>
 
-        <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
           <nav
             class="relative flex items-center justify-between sm:h-10 lg:justify-start"
             aria-label="Global"
@@ -24,10 +24,11 @@
             >
               <div class="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
-                  <span class="sr-only">Workflow</span>
+                  <span class="sr-only">Z-Works</span>
                   <img
-                    class="h-20 w-auto"
+                    class="w-auto h-20"
                     src="~/assets/images/Logo-Z-works-ce7b1ec2c09b71ee7b014c59c8d34d14.png"
+                    alt=""
                   />
                 </a>
               </div>
@@ -36,11 +37,11 @@
         </div>
 
         <main
-          class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+          class="px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
         >
           <div class="sm:text-center lg:text-left">
             <h1
-              class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+              class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl"
             >
               Professionele
               <span class="text-yellow-500">bedrijfsfilm</span> laten maken?
@@ -59,7 +60,7 @@
               <div class="rounded-md shadow">
                 <a
                   href="#"
-                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 md:py-4 md:text-lg md:px-10"
+                  class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-yellow-600 border border-transparent rounded-md hover:bg-yellow-700 md:py-4 md:text-lg md:px-10"
                 >
                   Offerte aanvragen
                 </a>
@@ -67,7 +68,7 @@
               <div class="mt-3 sm:mt-0 sm:ml-3">
                 <a
                   href="#"
-                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-yellow-700 bg-yellow-100 hover:bg-yellow-200 md:py-4 md:text-lg md:px-10"
+                  class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-yellow-700 bg-yellow-100 border border-transparent rounded-md hover:bg-yellow-200 md:py-4 md:text-lg md:px-10"
                 >
                   Contact
                 </a>
@@ -79,7 +80,7 @@
     </div>
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img
-        class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        class="object-cover w-full h-56 sm:h-72 md:h-96 lg:w-full lg:h-full"
         src="~/assets/images/marcos-rocha-_XgGN5yo1QE-unsplash-d18bd44552b956a6a3a7f6865399da0e.jpg"
         alt=""
       />
