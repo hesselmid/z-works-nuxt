e348bc90ce4ac141973e04fb69255a1bbf994e83
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-02-17 22:05:42 +0100

    fixed vimeo component bug

--- components/VimeoEmbed.vue
+++ components/VimeoEmbed.vue
@@ -2,7 +2,9 @@
   <div>
     <div style="padding:56.25% 0 0 0;position:relative;">
       <iframe
-        src="https://player.vimeo.com/video/506079196?title=0&byline=0&portrait=0"
+        :src="
+          `https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0`
+        "
         style="position:absolute;top:0;left:0;width:100%;height:100%;border-radius:15px;"
         frameborder="0"
         allow="autoplay; fullscreen; picture-in-picture"
@@ -12,3 +14,9 @@
     <script src="https://player.vimeo.com/api/player.js"></script>
   </div>
 </template>
+
+<script>
+export default {
+  props: ['id'],
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,54 +10,11 @@
           Meer dan 6.500 tevreden klanten en geproduceerde video’s
         </p>
         <div class="grid gap-8 mt-6 md:grid-cols-2 lg:mt-8">
-          <div>
-            <div style="padding:56.25% 0 0 0;position:relative;">
-              <iframe
-                src="https://player.vimeo.com/video/506079196?title=0&byline=0&portrait=0"
-                style="position:absolute;top:0;left:0;width:100%;height:100%;border-radius:15px;"
-                frameborder="0"
-                allow="autoplay; fullscreen; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </div>
-            <script src="https://player.vimeo.com/api/player.js"></script>
-          </div>
-          <div>
-            <div style="padding:56.25% 0 0 0;position:relative;">
-              <iframe
-                src="https://player.vimeo.com/video/506079293?title=0&byline=0&portrait=0"
-                style="position:absolute;top:0;left:0;width:100%;height:100%;border-radius:15px;"
-                frameborder="0"
-                allow="autoplay; fullscreen; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </div>
-            <script src="https://player.vimeo.com/api/player.js"></script>
-          </div>
-          <div>
-            <div style="padding:56.25% 0 0 0;position:relative;">
-              <iframe
-                src="https://player.vimeo.com/video/511581071?title=0&byline=0&portrait=0"
-                style="position:absolute;top:0;left:0;width:100%;height:100%;border-radius:15px;"
-                frameborder="0"
-                allow="autoplay; fullscreen; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </div>
-            <script src="https://player.vimeo.com/api/player.js"></script>
-          </div>
-          <div>
-            <div style="padding:56.25% 0 0 0;position:relative;">
-              <iframe
-                src="https://player.vimeo.com/video/506079156?title=0&byline=0&portrait=0"
-                style="position:absolute;top:0;left:0;width:100%;height:100%;border-radius:15px;"
-                frameborder="0"
-                allow="autoplay; fullscreen; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </div>
-            <script src="https://player.vimeo.com/api/player.js"></script>
-          </div>
+          <VimeoEmbed
+            v-for="vimeoId in vimeoIds"
+            :key="vimeoId"
+            v-bind="vimeoId"
+          />
         </div>
       </div>
     </div>
@@ -123,6 +80,7 @@ export default {
   },
   data() {
     return {
+      vimeoIds: [],
       logos: [],
       prices: [],
       faqs: [],
