53bdaea8eec6a3ab818d36a3ea44b9e03c5c2b77
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-02-18 13:54:53 +0100

    implemented lite vimeo

--- components/VimeoEmbed.vue
+++ components/VimeoEmbed.vue
@@ -1,19 +1,8 @@
 <template>
   <div style="padding:56.25% 0 0 0;position:relative;">
-    <!-- <div style="padding:56.25% 0 0 0;position:relative;">
-      <iframe
-        :src="
-          `https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0`
-        "
-        style="position:absolute;top:0;left:0;width:100%;height:100%;border-radius:15px;"
-        title="Z-Works video"
-        frameborder="0"
-        allowfullscreen
-      ></iframe>
-    </div>
-    <script src="https://player.vimeo.com/api/player.js"></script> -->
     <lite-vimeo
       :videoid="id"
+      videotitle="Z-Works video"
       style="position:absolute;top:0;left:0;width:100%;height:100%;border-radius:15px;"
     ></lite-vimeo>
   </div>
