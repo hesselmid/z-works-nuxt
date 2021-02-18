f6a81a40fe132b05a607cf194aba2ae3edbc432b
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-02-18 13:23:52 +0100

    added preconnect links to vimeo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,56 @@
 export default {
-  target: 'static',
+  target: "static",
   head: {
-    titleTemplate: '%s - Z-Works',
+    titleTemplate: "%s - Z-Works",
     htmlAttrs: {
-      lang: 'nl',
+      lang: "nl",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
+        hid: "description",
+        name: "description",
         content:
-          'Een bedrijfsvideo maakt impact. Op televisie, social media en je website. Eenmalige investering, eindeloos resultaat. Laat je bedrijf zien met een professionele bedrijfsfilm van Z-Works.',
+          "Een bedrijfsvideo maakt impact. Op televisie, social media en je website. Eenmalige investering, eindeloos resultaat. Laat je bedrijf zien met een professionele bedrijfsfilm van Z-Works.",
       },
       {
-        name: 'google-site-verification',
-        content: 'LZgGPqTpyccWLs25eOrUBjXZho3UQOPUaruuXa0r-Vo',
+        name: "google-site-verification",
+        content: "LZgGPqTpyccWLs25eOrUBjXZho3UQOPUaruuXa0r-Vo",
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "preconnect",
+        href: "https://player.vimeo.com",
+      },
+      {
+        rel: "preconnect",
+        href: "https://i.vimeocdn.com",
+      },
+      {
+        rel: "preconnect",
+        href: "https://f.vimeocdn.com",
+      },
+    ],
   },
   components: true,
-  buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-analytics', '@nuxt/image'],
+  buildModules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-analytics",
+    "@nuxt/image",
+  ],
   build: {
     html: {
       minify: {
         removeRedundantAttributes: false,
       },
     },
-    transpile: ['vue2-smooth-scroll'],
+    transpile: ["vue2-smooth-scroll"],
   },
-  plugins: ['~/plugins/vue2-smooth-scroll.js'],
+  plugins: ["~/plugins/vue2-smooth-scroll.js"],
   googleAnalytics: {
-    id: 'UA-136136233-11',
+    id: "UA-136136233-11",
   },
 };
