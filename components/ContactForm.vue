ad405bba625621a5290a2e5c3795a1c6b020ef56
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-02-18 13:05:20 +0100

    added lazy loading

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -7,6 +7,7 @@
           src="/gordon-cowie-tyuVOf1G3d0-unsplash.jpg"
           sizes="sm:640px md:768px lg:1024px xl:1280px"
           quality="80"
+          loading="lazy"
           alt="Opname bedrijfsvideo"
         />
       </div>
