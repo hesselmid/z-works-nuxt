f1972b5fbf18ad1ad0c73d048c1e734e81b860fb
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-02-18 11:28:16 +0100

    added title to iframes

--- components/VimeoEmbed.vue
+++ components/VimeoEmbed.vue
@@ -6,6 +6,7 @@
           `https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0`
         "
         style="position:absolute;top:0;left:0;width:100%;height:100%;border-radius:15px;"
+        title="Z-Works video"
         frameborder="0"
         allowfullscreen
       ></iframe>
