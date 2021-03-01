ef723f0f7197077a54efaa264d417cc7263bb864
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-03-01 12:34:58 +0100

    leadinfo poging 10

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,15 @@ export default {
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    __dangerouslyDisableSanitizers: ['script'],
+    script: [
+      {
+        hid: 'script',
+        innerHTML: `(function(l,e,a,d,i,n,f,o){if(!l[i]){l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[]; l.GlobalLeadinfoNamespace.push(i);l[i]=function(){(l[i].q=l[i].q||[]).push(arguments)};l[i].t=l[i].t||n; l[i].q=l[i].q||[];o=e.createElement(a);f=e.getElementsByTagName(a)[0];o.async=1;o.src=d;f.parentNode.insertBefore(o,f);} }(window,document,"script","https://cdn.leadinfo.net/ping.js","leadinfo","LI-6038FD7646E63"));`,
+        type: 'text/javascript',
+        charset: 'utf-8',
+      },
+    ],
   },
   components: true,
   buildModules: [
@@ -35,11 +44,7 @@ export default {
     },
     transpile: ['vue2-smooth-scroll'],
   },
-  plugins: [
-    '~/plugins/vue2-smooth-scroll.js',
-    '~/plugins/vimeo.client.js',
-    '~/plugins/leadinfo.client.js',
-  ],
+  plugins: ['~/plugins/vue2-smooth-scroll.js', '~/plugins/vimeo.client.js'],
   googleAnalytics: {
     id: 'UA-136136233-11',
   },
