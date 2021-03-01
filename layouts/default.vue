79561f11efc66602dc127e742b99165b2a67b840
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-03-01 12:56:08 +0100

    leadinfo poging 20

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,3 +4,15 @@
     <TheFooter />
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.innerHTML =
+      '(function(l,e,a,d,i,n,f,o){if(!l[i]){l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[]; l.GlobalLeadinfoNamespace.push(i);l[i]=function(){(l[i].q=l[i].q||[]).push(arguments)};l[i].t=l[i].t||n; l[i].q=l[i].q||[];o=e.createElement(a);f=e.getElementsByTagName(a)[0];o.async=1;o.src=d;f.parentNode.insertBefore(o,f);} }(window,document,"script","https://cdn.leadinfo.net/ping.js","leadinfo","LI-6038FD7646E63"));';
+    document.head.appendChild(script);
+  },
+};
+</script>
